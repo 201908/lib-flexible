50caa5713b6cd62de6a4509d2b0b7c0a5ab522de
联接git <chenteng0810@gmail.com>
2019-09-28 19:49:38 +0800

    hao

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-(function flexible (window, document) {
+(function flexible(window, document) {
   var docEl = document.documentElement
   var dpr = window.devicePixelRatio || 1
 
   // adjust body font size
-  function setBodyFontSize () {
+  function setBodyFontSize() {
     if (document.body) {
-      document.body.style.fontSize = (12 * dpr) + 'px'
-    }
-    else {
+      // document.body.style.fontSize = (12 * dpr) + 'px'
+    } else {
       document.addEventListener('DOMContentLoaded', setBodyFontSize)
     }
   }
   setBodyFontSize();
 
   // set 1rem = viewWidth / 10
-  function setRemUnit () {
-    var rem = docEl.clientWidth / 10
+  function setRemUnit() {
+    var width = docEl.clientWidth
+    if (width > 750) {
+      width = 750
+    }
+    var rem = width / 18.75
     docEl.style.fontSize = rem + 'px'
   }
 
